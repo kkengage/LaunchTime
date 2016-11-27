764009d86fbb1cab2f9a996adae4c8af4c0d6251
Zenbook3\admin <krawczyk01@gmail.com>
2016-11-27 17:49:36 +0100

    Konweretr grafiki

--- LogoConverter/MainWindow.xaml.cs
+++ LogoConverter/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,37 @@ namespace LogoConverter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {               
+                var minBrightnss = 128;
+                var sb = new StringBuilder();
+                var of = new OpenFileDialog();
+                of.DefaultExt = "*.bmp";
+                var f = of.ShowDialog();
+                if (f.HasValue && f.Value == true)
+                {
+                    Bitmap bmp = new Bitmap(of.FileName);
+                    for (var x = 0; x < bmp.Width; x++)
+                        for (var y = 0; y < (int)(bmp.Height / 8); y++)
+                        {
+                            var b = 0;
+                            for (var c = 0; c < 8; c++)
+                                b += bmp.GetPixel(x, y + c).R > minBrightnss ? (1 << c) : 0;
+                            if (sb.Length > 0) sb.Append(",");
+                            sb.Append(b.ToString());
+                        }
+                    textBox.Text = sb.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nie udalo sie otworzyc! \r\n\r\n" + ex.ToString());
+            }
         }
     }
 }
