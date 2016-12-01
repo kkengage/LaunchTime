7008547096ea0f0cbbc055988a66680afed4568b
Zenbook3\admin <krawczyk01@gmail.com>
2016-12-01 03:49:43 +0100

    Dzialajacy wydruk zamowienia!

--- LaunchTime/www/app/services/Printer/PrinterService.js
+++ LaunchTime/www/app/services/Printer/PrinterService.js
@@ -1,9 +1,9 @@
 /// <reference path="../../models/order.ts" />
 /// <reference path="printer.ts" />
 /// <reference path="../../../../typings/tsd.d.ts" />
+/// <reference path="../../appmain/app.ts" />
 var Printer;
 (function (Printer) {
-    // Rozszerza funckjonalność cache'a angularowego o expiry time
     var PrinterService = (function () {
         function PrinterService($q, toastr) {
             this.$q = $q;
@@ -13,15 +13,109 @@ var Printer;
         }
         PrinterService.prototype.PrintOrder = function (order) {
             var _self = this;
+            var print = this._printer;
             return this.$q(function (resolve, reject) {
-                /// drukuj dlugo....
-                try {
-                    _self._printer.PrintLogo().done(function () { _self.toastr.success("Pomyślnie wydrukowano zamówienie", "Zakończono wydruk"); }).fail(function (msg) { _self.toastr.error(msg, "Błąd wydruku"); });
-                }
-                catch (ex) {
-                    reject("Blad wydruku zamówienia!\r\n" + ex.toString());
-                }
+                print.PrintLogo().done(function () {
+                    return print.SetAlignment(Printer.TextAlignment.Center).done(function () {
+                        return print.SetFont(Printer.FontStyle.Width2).done(function () {
+                            return print.PrintText("ZAMÓWIENIE\r\n").done(function () {
+                                return print.SetFont(Printer.FontStyle.Normal).done(function () {
+                                    return print.PrintText("NR ").done(function () {
+                                        return print.SetFont(Printer.FontStyle.Width2).done(function () {
+                                            return print.PrintText(order.Id + "\r\n").done(function () {
+                                                return print.SetFont(Printer.FontStyle.Normal).done(function () {
+                                                    return print.SetUnderline(Printer.Underline.Thick).done(function () {
+                                                        return print.PrintText("DATA                     GODZINA\r\n").done(function () {
+                                                            return print.SetUnderline(Printer.Underline.None).done(function () {
+                                                                return print.SetFont(Printer.FontStyle.Height2).done(function () {
+                                                                    return print.PrintText(order.Date + "            ").done(function () {
+                                                                        return print.SetFont(Printer.FontStyle.Height2 | Printer.FontStyle.Width2 | Printer.FontStyle.Bold).done(function () {
+                                                                            return print.PrintText(order.Hour + "\r\n").done(function () {
+                                                                                return print.SetFont(Printer.FontStyle.Small).done(function () {
+                                                                                    return print.SetAlignment(Printer.TextAlignment.Left).done(function () {
+                                                                                        return print.PrintText("ZAMÓWIONE PRODUKTY:\r\n").done(function () {
+                                                                                            return _self.printItems(print, order, 0).done(function () {
+                                                                                                return _self.printSummary(print, order).done(function () {
+                                                                                                    return _self.toastr.success("Pomyślnie wydrukowano zamówienie", "Zakończono wydruk");
+                                                                                                });
+                                                                                            });
+                                                                                        });
+                                                                                    });
+                                                                                });
+                                                                            });
+                                                                        });
+                                                                    });
+                                                                });
+                                                            });
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                }).fail(function (msg) { return _self.toastr.success(msg, "Błąd wydruku raportu"); });
             });
+        };
+        PrinterService.prototype.printItems = function (print, order, id, deffer) {
+            if (deffer === void 0) { deffer = null; }
+            if (deffer == null)
+                deffer = $.Deferred();
+            var _self = this;
+            if (id >= order.Items.length) {
+                deffer.resolve();
+            }
+            else {
+                var item = order.Items[id];
+                print.SetFont(Printer.FontStyle.Bold | Printer.FontStyle.Height2 | Printer.FontStyle.Width2).done(function () {
+                    return print.PrintText(item.Quantity + "x  ").done(function () {
+                        return print.SetFont(Printer.FontStyle.Height2).done(function () {
+                            return print.PrintText(item.Name + "\r\n").done(function () { return _self.printItems(print, order, id + 1, deffer); });
+                        });
+                    });
+                }).fail(function (msg) { return deffer.reject(msg); });
+            }
+            if (id == 0)
+                return deffer.promise(); // zwroc promise tylko dla pierwszego wywolania
+            return null;
+        };
+        PrinterService.prototype.printSummary = function (print, order) {
+            var deffer = $.Deferred();
+            var _self = this;
+            print.SetFont(Printer.FontStyle.Width2).done(function () {
+                return print.SetInvertedColors(true).done(function () {
+                    return print.PrintText("SUMA:   123,00zl\r\n").done(function () {
+                        return print.SetInvertedColors(false).done(function () {
+                            return print.SetFont(Printer.FontStyle.Small).done(function () {
+                                return print.SetAlignment(Printer.TextAlignment.Center).done(function () {
+                                    return print.PrintText("ADRES ZAMÓWIENIA:\r\n").done(function () {
+                                        return print.SetFont(Printer.FontStyle.Height2).done(function () {
+                                            return print.PrintText(order.Client.Name + "\r\n" + order.Client.Address + "\r\n").done(function () {
+                                                return print.SetFont(Printer.FontStyle.Small).done(function () {
+                                                    return print.PrintText("NUMER TELEFONU:\r\n").done(function () {
+                                                        return print.SetFont(Printer.FontStyle.Height2 | Printer.FontStyle.Bold).done(function () {
+                                                            return print.PrintText(order.Client.Phone + "\r\n\r\n\r\n\r\n").done(function () {
+                                                                return deffer.resolve();
+                                                            });
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            }).fail(function (msg) { return deffer.reject(msg); });
+            return deffer.promise();
+        };
+        //_.reduce(order.Items, (memo, itm) => { return memo + (itm.Price * itm.Quantity); }, 0))
+        PrinterService.prototype.blad = function (msg) {
         };
         PrinterService.prototype.PrintReport = function (order) {
             var _self = this;
--- LaunchTime/www/app/services/Printer/printerService.ts
+++ LaunchTime/www/app/services/Printer/printerService.ts
@@ -1,10 +1,11 @@
 ﻿/// <reference path="../../models/order.ts" />
 /// <reference path="printer.ts" />
 /// <reference path="../../../../typings/tsd.d.ts" />
+/// <reference path="../../appmain/app.ts" />
 
 
 namespace Printer {
-    // Rozszerza funckjonalność cache'a angularowego o expiry time
+
     export class PrinterService {
 
         static $inject = ['$q', 'toastr'];
@@ -21,16 +22,77 @@ namespace Printer {
 
         PrintOrder(order: Models.Order): angular.IPromise<string> {
             var _self = this;
+            var print = this._printer;
             return this.$q(function (resolve, reject) {
-                /// drukuj dlugo....
-                try {
-                    _self._printer.PrintLogo().done(() => { _self.toastr.success("Pomyślnie wydrukowano zamówienie" , "Zakończono wydruk") }).fail((msg) => { _self.toastr.error(msg, "Błąd wydruku"); });
-                    //alert("PRINTINTG ORDER");
-                    //resolve(true);
-                } catch (ex) {
-                    reject("Blad wydruku zamówienia!\r\n" + ex.toString());
-                }
+                print.PrintLogo().done(() =>
+                    print.SetAlignment(TextAlignment.Center).done(() =>
+                        print.SetFont(FontStyle.Width2).done(() =>
+                            print.PrintText("ZAMÓWIENIE\r\n").done(() =>
+                                print.SetFont(FontStyle.Normal).done(() =>
+                                    print.PrintText("NR ").done(() =>
+                                        print.SetFont(FontStyle.Width2).done(() =>
+                                            print.PrintText(order.Id + "\r\n").done(() =>
+                                                print.SetFont(FontStyle.Normal).done(() =>
+                                                    print.SetUnderline(Underline.Thick).done(() =>
+                                                        print.PrintText("DATA                     GODZINA\r\n").done(() =>
+                                                            print.SetUnderline(Underline.None).done(() =>
+                                                                print.SetFont(FontStyle.Height2).done(() =>
+                                                                    print.PrintText(order.Date + "            ").done(() => // 10 znakow data + 12 znakow przerwy
+                                                                        print.SetFont(FontStyle.Height2 | FontStyle.Width2 | FontStyle.Bold).done(() =>
+                                                                            print.PrintText(order.Hour + "\r\n").done(() => // 5znakow godzin * 2x szerokośc = 10 znaków
+                                                                                print.SetFont(FontStyle.Small).done(() =>
+                                                                                    print.SetAlignment(TextAlignment.Left).done(() =>
+                                                                                        print.PrintText("ZAMÓWIONE PRODUKTY:\r\n").done(() =>
+                                                                                            _self.printItems(print, order, 0).done(() =>
+                                                                                                _self.printSummary(print, order).done(() =>
+                                                                                                    _self.toastr.success("Pomyślnie wydrukowano zamówienie", "Zakończono wydruk")
+                                                                                                ))))))))))))))))))))).fail((msg) => _self.toastr.success(msg, "Błąd wydruku raportu"));
             });
+        }
+
+        printItems(print: Printer, order: Models.Order, id: number, deffer: JQueryDeferred<string> = null): JQueryPromise<string> {
+            if (deffer == null) deffer = $.Deferred<string>();
+            var _self = this;
+
+            if (id >= order.Items.length) { deffer.resolve(); }
+            else {
+                var item = order.Items[id];
+                print.SetFont(FontStyle.Bold | FontStyle.Height2 | FontStyle.Width2).done(() =>
+                    print.PrintText(item.Quantity + "x  ").done(() =>
+                        print.SetFont(FontStyle.Height2).done(() =>
+                            print.PrintText(item.Name + "\r\n").done(() => _self.printItems(print, order, id + 1, deffer))
+                        ))).fail((msg) => deffer.reject(msg));
+            }
+            if (id == 0) return deffer.promise(); // zwroc promise tylko dla pierwszego wywolania
+            return null;
+        }
+
+        printSummary(print: Printer, order: Models.Order): JQueryPromise<string> {
+            var deffer = $.Deferred<string>();
+            var _self = this;
+            print.SetFont(FontStyle.Width2).done(() =>
+                print.SetInvertedColors(true).done(() =>
+                    print.PrintText("SUMA:   123,00zl\r\n").done(() =>
+                        print.SetInvertedColors(false).done(() =>
+                            print.SetFont(FontStyle.Small ).done(() =>
+                                print.SetAlignment(TextAlignment.Center).done(()=>
+                                print.PrintText("ADRES ZAMÓWIENIA:\r\n").done(() =>
+                                    print.SetFont(FontStyle.Height2).done(() =>
+                                        print.PrintText(order.Client.Name + "\r\n" + order.Client.Address + "\r\n").done(() =>
+                                            print.SetFont(FontStyle.Small).done(() =>
+                                                print.PrintText("NUMER TELEFONU:\r\n").done(() =>
+                                                    print.SetFont(FontStyle.Height2 | FontStyle.Bold).done(() =>
+                                                        print.PrintText(order.Client.Phone+"\r\n\r\n\r\n\r\n").done(() =>
+                                                                deffer.resolve()
+                                                            ))))))))))))).fail((msg) => deffer.reject(msg));
+            return deffer.promise();
+        }
+
+
+        //_.reduce(order.Items, (memo, itm) => { return memo + (itm.Price * itm.Quantity); }, 0))
+
+        private blad(msg: string) {
+
         }
 
         PrintReport(order: Models.Order): angular.IPromise<string> {
